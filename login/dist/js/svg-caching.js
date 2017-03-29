602b64b72992306be211764972dad601e1343bf2
Olegario <l0s-c0ldest@mail.ru>
2017-03-30 00:27:56 +0300

    Update svg-caching.js

--- login/dist/js/svg-caching.js
+++ login/dist/js/svg-caching.js
@@ -2,7 +2,7 @@
 {
 	'use strict';
 
-	var file     = '../svg-sprite.svg',
+	var file     = '../dist/svg-sprite.svg',
 		revision = 1;
 
 	if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
